f9b3163dca565235d33e392416beb0d86dfe3cf5
KhanhDT2410 <khduong.work@gmail.com>
2024-08-12 15:49:41 +0700

    1.2

--- src/App.java
+++ src/App.java
@@ -2,5 +2,11 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Konnichiwa");
+
+
+
+        int x = 1;
+        x++;
+        System.out.println(x);
     }
 }
